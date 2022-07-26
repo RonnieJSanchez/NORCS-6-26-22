8b85171e0836835778a743d6f31c792ee5e5e1fe
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-26 19:50:41 -0400

    code along

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,3 +1,4 @@
-export default function Card(){
-    return <>Card</>;
+
+export default function Card({cardValue}){
+    return <div className="card" style={{height:"120px", width:"80px", border:"1px solid black"}}>{cardValue}</div>;
 }
